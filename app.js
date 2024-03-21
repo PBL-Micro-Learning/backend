e1fc3ad692c4490ec41bf0bb7332b073215130af
Aryo <aryoyudho29@gmail.com>
2024-03-21 14:19:16 +0700

    feat: add api-docs

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
-const app = express();
+const swaggerUi = require('swagger-ui-express');
+const fs = require("fs");
+const YAML = require('yaml');
 
+const file = fs.readFileSync('./swagger.yaml', 'utf8');
+const swaggerDocument = YAML.parse(file);
+
+const app = express();
 app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -10,6 +16,7 @@ app.set('view engine', 'ejs');
 
 const router = require('./routes/index.js');
 app.use('/api', router);
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.get('/', function (req, res) {
     res.json({
